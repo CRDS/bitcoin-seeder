0f0332887aa8098e72bcd7350037ad66a078254c
mdrankin <mr3801@gmail.com>
2017-09-18 19:33:32 +1000

    Further update to all files
    Updated paramaters for Credits in Multiple files.

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x2f, 0x32, 0x45, 0x51 };
 
 CMessageHeader::CMessageHeader()
 {
